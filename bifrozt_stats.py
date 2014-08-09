c457914fbc7ab753311870974e0b5c90374c2923
ZombieNinjaPirate <black.september@me.com>
2014-08-09 15:18:07 +0200

    Added a few more arguments, might have to debuf them a bit...

--- bifrozt_stats.py
+++ bifrozt_stats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-"""This script allows you to extract data sets from the HonSSH log files. """
+"""This script allows you to extract data from the log files on Bifrozt. """
 
 """
 Copyright (c) 2014, Are Hansen - Honeypot Development.
@@ -30,157 +30,63 @@ THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 __author__ = 'Are Hansen'
 __date__ = '2014, May 15'
-__version__ = '0.1.7'
+__version__ = '0.1.8'
 
 
 import argparse
 import os
 import sys
-from Bifrozt.FileProcessing.Read import filelines
-from Bifrozt.Find.Addresses import IPv4, IPv4part
-from Bifrozt.Find.Files import locate
-from Bifrozt.Find.LogData import startEnd
-from Bifrozt.Count.Lists import element, nrOfItems, uniqElements
-from Bifrozt.Find.IPgeo import cname
-from Bifrozt.HonSSH.DailyLogs import sourceIPv4, passwords, users, combos, access
-from Bifrozt.HonSSH.GEO import accessCC
-from Bifrozt.HonSSH.Output import source, origin, passwd, usrnames, combinations, foundlogin, summary
+from Bifrozt.CmdProcessing.Stats import honsshData, firewallData, dataSummary
 
 
 def parse_args():
     """Defines the command line arguments. """
-    hlog = '/opt/honssh/logs'
-    parser = argparse.ArgumentParser('Data extraction script')
+    parser = argparse.ArgumentParser('Bifrozt data mining')
 
-    honssh = parser.add_argument_group('- HonSSH data')
+    honssh = parser.add_argument_group('HonSSH data')
+    honssh.add_argument('-SUM', dest='summry', help='HonSSH summary', action='store_true')
     honssh.add_argument('-A', dest='access', help='Valid login found', action='store_true')
     honssh.add_argument('-S', dest='source', help='Connection/IP address', action='store_true')
     honssh.add_argument('-O', dest='origin', help='Connection/country', action='store_true')
     honssh.add_argument('-P', dest='passwd', help='Frequent passwords', action='store_true')
     honssh.add_argument('-U', dest='usrnam', help='Frequent usernames', action='store_true')
     honssh.add_argument('-C', dest='combos', help='Frequent combinations', action='store_true')
-    honssh.add_argument('-SUM', dest='summry', help='Print summary', action='store_true')
-
-    search = parser.add_argument_group('- Show data based on a shared object')
-    search.add_argument('-QP', dest='qpasswd', help='Show passwords used by IP or octet(s) in IP',
+    data = honssh.add_mutually_exclusive_group()
+    data.add_argument('-QP', dest='qpasswd', help='Show passwords used by IP or octet(s) in IP',
                         nargs=1, type=str)
-    search.add_argument('-QU', dest='qusrnam', help='Show usernames used by IP or octet(s) in IP',
+    data.add_argument('-QU', dest='qusrnam', help='Show usernames used by IP or octet(s) in IP',
                         nargs=1, type=str)
-    search.add_argument('-QC', dest='qcombos', help='Show combos used by IP or octet(s) in IP',
+    data.add_argument('-QC', dest='qcombos', help='Show combos used by IP or octet(s) in IP',
                         nargs=1, type=str)
 
-    out = parser.add_argument_group('- Output control')
+    fwlogs = parser.add_argument_group('Firewall data')
+    fwlogs.add_argument('-HTTP', dest='fwhttp', help='Show HTTP destinations', action='store_true')
+    fwlogs.add_argument('-IRC', dest='fwirc', help='Show IRC destinations', action='store_true')
+
+    logdirs = parser.add_argument_group('Log directories')
+    logs = logdirs.add_mutually_exclusive_group()
+    logs.add_argument('-HL', dest='hondir', help='HonSSH log directory', nargs=1)
+    logs.add_argument('-FL', dest='fwldir', help='Firewall log directory', nargs=1)
+
+    out = parser.add_argument_group('Output control')
     out.add_argument('-n', dest='number', help='Number of lines displayed (default: 50)')
-
-    logs = parser.add_argument_group('- Log directory')
-    logs.add_argument('-H', dest='hondir', help='HonSSH logs ({0})'.format(hlog), default=hlog)
-
+    
     args = parser.parse_args()
+
     return args
 
 
 def process_args(args):
     """Process the command line arguments."""
-    logfiles = []
-    loglines = []
-    number = 50
-    
-    if args.hondir and len(sys.argv) <= 1:
-        print len(sys.argv)
-        print '\nUSAGE: {0} -h\n'.format(sys.argv[0].split('/')[-1])
-        sys.exit(1)
-
-    if not args.hondir and len(sys.argv) < 1:
-        print '\nUSAGE: {0} -h\n'.format(sys.argv[0].split('/')[-1])
-        sys.exit(1)
-
     if args.hondir:
-        if not os.path.isdir(args.hondir):
-            print 'ERROR: {0} does not appear to exist!'.format(args.hondir)
-            sys.exit(1)
-        honssh_logs = filelines(locate(args.hondir, '20'))
-    else:
-        honssh_logs = filelines(locate(args.hondir, '20'))
-    
-    if args.number:
-        number = int(args.number)
+        honsshData(args)
 
-    for log, logdata in honssh_logs.items():
-        logfiles.append(log)
+    if args.fwldir:
+         firewallData(args)
 
-        for lines in logdata:
-            loglines.append(lines.rstrip())
-
-    if args.source:
-        sourceips = sourceIPv4(loglines)
-        countdips = element(sourceips, None)
-        source(countdips, number)
-
-    if args.origin:
-        sourceips = sourceIPv4(loglines)
-        findcname = cname(sourceips)
-        countname = element(findcname, None)
-        origin(countname, number)
-
-    if args.passwd:
-        usedpasswd = passwords(loglines)
-        pass_items = element(usedpasswd, None)
-        passwd(pass_items, number)
-
-    if args.usrnam:
-        usedunames = users(loglines)
-        user_items = element(usedunames, None)
-        usrnames(user_items, number)
-
-    if args.combos:
-        attemptedc = combos(loglines)
-        comb_items = element(attemptedc, None)
-        combinations(comb_items, number)
-
-    if args.access:
-        gainaccess = access(loglines)
-        geoipslook = accessCC(gainaccess)
-        foundlogin(geoipslook, number)
-
-    if args.summry:
-        logstimed = startEnd(loglines, ',', 1)
-        attacknum = nrOfItems(loglines)
-        sourceips = sourceIPv4(loglines)
-        uniqueips = uniqElements(sourceips)
-        ipv4numbr = nrOfItems(uniqueips)
-        findcname = cname(sourceips)
-        countname = element(findcname, None)
-        countrynr = nrOfItems(countname.keys())
-        usedunames = users(loglines)
-        user_items = element(usedunames, None)
-        uniqusrnam = uniqElements(user_items)
-        usernamynr = nrOfItems(uniqusrnam)
-        usedpasswd = passwords(loglines)
-        pass_items = element(usedpasswd, None)
-        uniqpasswd = nrOfItems(pass_items.keys())
-        attemptedc = combos(loglines)
-        comb_items = element(attemptedc, None)
-        uniqcombos = nrOfItems(comb_items.keys())
-        summary(logstimed, attacknum, ipv4numbr, countrynr, 
-                usernamynr, uniqpasswd, uniqcombos)
-
-    if args.qpasswd:
-        searchdata = IPv4part(args.qpasswd, loglines)
-        querpasswd = passwords(searchdata)
-        pass_items = element(querpasswd, None)
-        passwd(pass_items, number)
-
-    if args.qusrnam:
-        searchdata = IPv4part(args.qusrnam, loglines)
-        querunames = users(loglines)
-        user_items = element(querunames, None)
-        usrnames(user_items, number)
-
-    if args.qcombos:
-        searchdata = IPv4part(args.qcombos, loglines)
-        attemptedc = combos(searchdata)
-        comb_items = element(attemptedc, None)
-        combinations(comb_items, number)
+    if args.fwldir == None and args.hondir == None:
+        print 'ERROR: You have to specify a log directory!'
+        sys.exit(1)
 
 
 def main():
